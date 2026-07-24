06bac7eb55860b88443777aac861daf73ba08690
Kipserem L <161978150+Achegei@users.noreply.github.com>
2026-07-24 14:02:38 +0300

    Choose your university

--- resources/views/auth/choose-university.blade.php
+++ resources/views/auth/choose-university.blade.php
@@ -13,7 +13,7 @@
 
         <div class="mb-8 text-center">
             <h1 class="text-3xl font-bold text-gray-900">
-                Choose Your University
+                Choose Your Institution/College/University
             </h1>
 
             <p class="text-gray-600 mt-2">
