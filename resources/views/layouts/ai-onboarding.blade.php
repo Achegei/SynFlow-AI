203a37eef774c6550bb2a065b8c865aea47f40ae
Kipserem L <161978150+Achegei@users.noreply.github.com>
2026-08-13 13:47:51 +0300

    Add Meta Pixel to AI onboarding

--- resources/views/layouts/ai-onboarding.blade.php
+++ resources/views/layouts/ai-onboarding.blade.php
@@ -23,6 +23,30 @@
         'resources/js/app.js'
     ])
 
+    <!-- Meta Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '1055122183931015');
+    fbq('track', 'PageView');
+</script>
+
+<noscript>
+    <img
+        height="1"
+        width="1"
+        style="display:none"
+        src="https://www.facebook.com/tr?id=1055122183931015&ev=PageView&noscript=1"
+    />
+</noscript>
+<!-- End Meta Pixel Code -->
 </head>
 
 <body class="bg-gray-50 text-gray-900 antialiased">
